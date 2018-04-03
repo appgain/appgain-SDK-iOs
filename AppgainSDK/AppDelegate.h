80769e9fd1101e6493da56f31439be643f9caa0a
Ragaie alfy <Ragaie@ehabs-bc-macbook-pro.local>
2018-04-03 13:48:35 +0200

    Initial Commit

---
+++ AppgainSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppgainSDK
+//
+//  Created by Ragaie alfy on 4/3/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppgainSDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppgainSDK
+//
+//  Created by Ragaie alfy on 4/3/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
