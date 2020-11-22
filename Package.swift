f9ddbe63b8244111c529b74c97b4f501b73c871e
Ragaie alfy Fahmey <eng.ragaie@gmail.com>
2020-11-22 17:08:59 +0200

    --

--- Package.swift
+++ Package.swift
@@ -18,16 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-   
         .target(
             name: "Appgain",
-            dependencies: [],
-         //   path: "AppgainSDK/", // 3
-
-            exclude: ["Appgain-Rich.podspec"]),
-       
-        .testTarget(
-            name: "AppgainTests",
-            dependencies: ["Appgain"]),
+            dependencies: [], path: "AppgainSDK/",   exclude: ["Appgain-Rich.podspec","Appgain.podspec","AppgainRich.podspec","LICENSE","AppgainSDK/Appgain-rich"])
+      
     ]
 )
